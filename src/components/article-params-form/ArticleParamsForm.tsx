2e59368a01406180c3231aae0c74b0ec14baef51
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-10-31 18:42:43 +0300

    Добавлено закрытие aside при клике не на нем

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,27 +1,41 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import clsx from 'clsx';
 import { Text } from 'src/ui/text';
 import { Select } from 'src/ui/select';
+import { useOutsideClickClose } from 'src/ui/select/hooks/useOutsideClickClose';
+import { Separator } from 'src/ui/separator';
+import { RadioGroup } from 'src/ui/radio-group';
 import styles from './ArticleParamsForm.module.scss';
 import {
 	ArticleStateType,
+	backgroundColors,
+	contentWidthArr,
 	defaultArticleState,
 	fontSizeOptions,
 	fontFamilyOptions,
 } from 'src/constants/articleProps';
-import { RadioGroup } from 'src/ui/radio-group';
 
 export const ArticleParamsForm = () => {
 	const [formOpen, setFormOpen] = useState<boolean>(false);
 	const [formState, setFormState] =
 		useState<ArticleStateType>(defaultArticleState);
+	const asideRef = useRef<HTMLDivElement>(null);
+
+	// хук для закрытия формы при клике не на ней
+	useOutsideClickClose({
+		isOpen: formOpen,
+		rootRef: asideRef,
+		onClose: () => setFormOpen(false),
+		onChange: setFormOpen,
+	});
 
 	return (
 		<>
 			<ArrowButton isOpen={formOpen} onClick={() => setFormOpen(!formOpen)} />
 			<aside
+				ref={asideRef} // Добавляем ссылку на aside
 				className={clsx(styles.container, formOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<Text size={31} weight={800} uppercase={true}>
@@ -42,6 +56,23 @@ export const ArticleParamsForm = () => {
 						title='размер шрифта'
 						onChange={(value) =>
 							setFormState({ ...formState, fontSizeOption: value })
+						}
+					/>
+					<Separator />
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						title='цвет фона'
+						onChange={(value) =>
+							setFormState({ ...formState, backgroundColor: value })
+						}
+					/>
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						title='Ширина контента'
+						onChange={(value) =>
+							setFormState({ ...formState, contentWidth: value })
 						}
 					/>
 					<div className={styles.bottomContainer}>
