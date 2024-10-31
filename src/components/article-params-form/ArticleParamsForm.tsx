8c1a5bef585e6398da7166970d45013dbdba3772
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-10-31 17:48:49 +0300

    Добавлен селект в форму и отредактирован отсуп на форме

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,10 +2,19 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 import { useState } from 'react';
 import clsx from 'clsx';
+import { Text } from 'src/ui/text';
+import { Select } from 'src/ui/select';
 import styles from './ArticleParamsForm.module.scss';
+import {
+	ArticleStateType,
+	defaultArticleState,
+	fontFamilyOptions,
+} from 'src/constants/articleProps';
 
 export const ArticleParamsForm = () => {
 	const [FormOpen, setFormOpen] = useState<boolean>(false);
+	const [formState, setFormState] =
+		useState<ArticleStateType>(defaultArticleState);
 
 	return (
 		<>
@@ -13,6 +22,17 @@ export const ArticleParamsForm = () => {
 			<aside
 				className={clsx(styles.container, FormOpen && styles.container_open)}>
 				<form className={styles.form}>
+					<Text size={31} weight={800} uppercase={true}>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						title='шрифт'
+						onChange={(value) =>
+							setFormState({ ...formState, fontFamilyOption: value })
+						}
+					/>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
