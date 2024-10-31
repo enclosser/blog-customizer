9389954ab67c1a8008e7e9b47ffab33122890406
andrei.antonov <andrei.antonov@corp.mail.ru>
2024-10-31 17:27:51 +0300

    Добавлено открытие формы по клику кнопки

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,17 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
-
+import { useState } from 'react';
+import clsx from 'clsx';
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [FormOpen, setFormOpen] = useState<boolean>(false);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton isOpen={FormOpen} onClick={() => setFormOpen(!FormOpen)} />
+			<aside
+				className={clsx(styles.container, FormOpen && styles.container_open)}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
